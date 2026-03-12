5cd128704f366a63072295f32f42fb496631ec49
“Nokuthula <chirwanokuthula@gmail.com>
2026-03-12 09:25:04 +0200

    The changes to 'alx_book_store.sql' have been saved.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -40,10 +40,10 @@ CREATE TABLE `Order_Details` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Orders` (
-  `order_id ` INT NOT NULL,
-  `customer_id ` INT NOT NULL,
-  order_date DATE DEFAULT NULL,
-  PRIMARY KEY (`order_id `),
+  `order_id` INT NOT NULL,
+  `customer_id` INT NOT NULL,
+  'order_date' DATE DEFAULT NULL,
+  PRIMARY KEY (`order_id`),
   KEY `customer_id ` (`customer_id `),
-  CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`customer_id `) REFERENCES Customers (`customer_id`)
+  CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`customer_id `) REFERENCES 'Customers' (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
